4ceb09a1f8ee04690bc1fb7380a880c0a4fc29f8
Filipe Pires <liipepires@gmail.com>
2018-07-09 04:46:03 -0300

    [ADD] config app

--- LabarcRiscV/App.js
+++ LabarcRiscV/App.js
@@ -1,55 +1,41 @@
-import React from 'react';
-import { createStackNavigator } from 'react-navigation';
-
-import Principal from './src/Screen/Principal';
-
-import OqueScreen from './src/Screen/Oque';
-import ComandoScreen from './src/Screen/Comandos';
-import TerminalScreen from './src/Screen/Terminal';
-import BaseInstructionScreen from './src/Screen/BaseInstruction';
+import { createStackNavigator } from 'react-navigation'
+import Principal from './src/Screen/Principal'
+import OqueScreen from './src/Screen/Oque'
+import ComandoScreen from './src/Screen/Comandos'
+import TerminalScreen from './src/Screen/Terminal'
+import BaseInstructionScreen from './src/Screen/BaseInstruction'
 
 const App = createStackNavigator({
   Home: {
     screen: Principal,
-    navigationOptions:{
-      title: 'Home',
-    },
+    navigationOptions: {
+      title: 'Home'
+    }
   },
   Terminal: {
     screen: TerminalScreen,
-    navigationOption : {
-      title: 'Terminal de Comandos',
-    },
+    navigationOptions: {
+      title: 'Terminal'
+    }
   },
   Intrucoes: {
     screen: BaseInstructionScreen,
-    navigationOptions:{
-      title: 'Conversor',
-    },
+    navigationOptions: {
+      title: 'Conversor'
+    }
   },
   Oque: {
     screen: OqueScreen,
-    navigationOptions:{
-      title: 'O que é?',
-    },
+    navigationOptions: {
+      title: 'O que é?'
+    }
   },
   Comandos: {
     screen: ComandoScreen,
-    navigationOptions:{
-      title: 'Comandos',
-    },
-  },
-});
+    navigationOptions: {
+      title: 'Comandos'
+    }
+  }
+})
 
-export default App;
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <AppNavigator />
-//       // <InstrucoesScreen />
-//     );
-//   }
-// }
-
-//cores ufcg : claro{ 3399cc } e { 330066 }
+export default App
